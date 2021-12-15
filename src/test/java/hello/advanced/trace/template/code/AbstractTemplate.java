fa126f7ef401748cd685961a042ca1715259a35a
HyeonWoo Choi <escape0619@gmail.com>
2021-12-15 17:26:14 +0900

    TemplateMethod pattern 적용(abstract class 정의 및 구현.)

---
+++ src/test/java/hello/advanced/trace/template/code/AbstractTemplate.java
@@ -0,0 +1,18 @@
+package hello.advanced.trace.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class AbstractTemplate {
+    public void execute(){
+        long startTime = System.currentTimeMillis();
+        // 비즈니스실행
+        call(); //-- 템플릿 (거대한 기준)
+        // 비즈니스 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    protected abstract void call();
+}
