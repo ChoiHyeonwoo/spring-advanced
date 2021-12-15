78b6a97dd58f06189cd9419de5bd63ce99ca94f4
HyeonWoo Choi <escape0619@gmail.com>
2021-12-15 17:38:36 +0900

    익명 클래스로 v2 적용

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -43,4 +43,25 @@ public class TemplateMethodTest {
         AbstractTemplate template2 = new SubclassLogic2();
         template2.execute();
     }
+
+    @Test
+    void templateMethodV2(){
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        };
+        log.info("클래스 이름2={}", template2.getClass());
+        template2.execute();
+    }
 }
