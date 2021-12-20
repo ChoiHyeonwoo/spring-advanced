a417edd51a05b9ee071bdc84fe56924f681f47c7
HyeonWoo Choi <escape0619@gmail.com>
2021-12-20 10:20:27 +0900

    TemplateCallback pattern 적용.

---
+++ src/main/java/hello/advanced/aop/v5/OrderControllerV5.java
@@ -0,0 +1,31 @@
+package hello.advanced.aop.v5;
+
+import hello.advanced.trace.callback.TraceCallback;
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderControllerV5 {
+
+    private final OrderServiceV5 orderServiceV5;
+
+    private final TraceTemplate template;
+
+    public OrderControllerV5(OrderServiceV5 orderServiceV5, LogTrace trace) {
+        this.orderServiceV5 = orderServiceV5;
+        this.template = new TraceTemplate(trace);
+    }
+
+    @GetMapping("/v5/request")
+    public String request(String itemId) {
+        return template.execute("OrderController.request()", new TraceCallback<>() {
+            @Override
+            public String call() {
+                orderServiceV5.orderItem(itemId);
+                return "OK";
+            }
+        });
+    }
+}
